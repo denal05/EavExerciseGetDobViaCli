8c3240986e451b74488cf10eb1ce78cb04dadbdf
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2024-11-16 22:36:34 +0100

    Followed CLI example at https://developer.adobe.com/commerce/php/development/cli-commands/custom/

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Denal05_EavExerciseGetDobViaCli',
+    __DIR__
+);
