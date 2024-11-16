673bb575696e0e9c279f8dbc35798589057db5e7
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2024-11-16 22:36:34 +0100

    Added a comment in RawSqlQuery explaining how to get the customer Date of Birth using the Entity-Attribute-Value data model. If we need the DoB (attribute) of a customer (entity) we should retrieve the customer ID using their email by querying the customer_entity table, the dob attribute ID in the eav_attribute table, and finally use both the entity_id and attribute_id to retrieve the DoB (value) in the customer_entity_datetime table. However, the example is old, and dob exists directly in the customer_entity table.

--- Helper/RawSqlQuery.php
+++ Helper/RawSqlQuery.php
@@ -11,40 +11,24 @@ use Magento\Framework\App\ResourceConnection;
 class RawSqlQuery extends AbstractHelper
 {
     protected $resourceConnection;
+
     public function __construct(Context $context, ResourceConnection $resourceConnection)
     {
         $this->resourceConnection = $resourceConnection;
         parent::__construct($context);
     }
+
     public function runSqlQueryOnTable($table)
     {
         $connection = $this->resourceConnection->getConnection();
         $table = $connection->getTableName($table);
+
+        // select entity_id from customer_entity where email="roni_cost@example.com";
+        // select attribute_id from eav_attribute where attribute_code='dob'; <- unnecessary
+        // select dob from customer_entity where entity_id=1;
+
         $query = "SELECT * FROM " . $table;
         $result = $connection->fetchAll($query);
         return $result;
-	/*
-        $this->_logger->log(print_r($result, true));
-        $id = 2;
-        $query = "SELECT * FROM `" . $table . "` WHERE id = $id ";
-        $result1 = $connection->fetchAll($query);
-        $this->_logger->log(print_r($result1, true));
-        //For Insert query
-        $tableColumn = ['id', 'name', 'age'];
-        $tableData[] = [5, 'xyz', '20'];
-        $connection->insertArray($table, $tableColumn, $tableData);
-        $query = "INSERT INTO `" . $table . "`(`id`, `name`, `age`) VALUES (7,'mtm',33)";
-        $connection->query($query);
-        // For Update query
-        $id = 1;
-        $query = "UPDATE `" . $table . "` SET `name`= 'test' WHERE id = $id ";
-        $connection->query($query);
-        $query1 = "UPDATE `" . $table . "` SET `name`= 'test', `age` = 14 WHERE id = $id ";
-        $connection->query($query1);
-        // For Delete query
-        $id = 1;
-        $query = "DELETE FROM `" . $table . "` WHERE id = $id ";
-        $connection->query($query);
-	 */
     }
 }
