d263c5b24a882a8f743e60a8843f958b7901f688
Denis Aleksandrov <denis.v.aleksandrov@gmail.com>
2024-11-18 09:17:48 +0100

    Added a comment in RawSqlQuery.php

--- Helper/RawSqlQuery.php
+++ Helper/RawSqlQuery.php
@@ -22,6 +22,15 @@ class RawSqlQuery extends AbstractHelper
     {
         $connection = $this->resourceConnection->getConnection();
 
+	/*
+	 * How to get the customer Date of Birth using the Entity-Attribute-Value data model:
+	 * If we need the DoB (attribute) of a customer (entity) we should retrieve the
+	 * customer ID using their email by querying the customer_entity table, the dob 
+	 * attribute ID in the eav_attribute table, and finally use both the entity_id and 
+	 * attribute_id to retrieve the DoB (value) in the customer_entity_datetime table. 
+	 * However, the example is old, and dob exists directly in the customer_entity table.
+	 */
+
         // select entity_id from customer_entity where email="roni_cost@example.com";
         // select attribute_id from eav_attribute where attribute_code='dob'; <- unnecessary
         // select dob from customer_entity where entity_id=1;
